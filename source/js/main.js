821f0f71619be59a42ec7a6b7a5cf58618a0845e
mintsweet <0x1304570@gmail.com>
2023-09-26 15:21:57 +1300

    refactor: clean dir

--- source/js/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-  const btn = document.querySelector('.menu-controller');
-  const body = document.body;
-
-  btn.addEventListener('click', function() {
-    btn.classList.toggle('active');
-    body.classList.toggle('offset');
-  });
-}
